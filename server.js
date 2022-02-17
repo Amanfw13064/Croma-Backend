3b6f637c5dc298a4ffdfe939b5b0a7af371cf695
Amanfw13064 <93372257+Amanfw13064@users.noreply.github.com>
2022-02-17 20:30:54 +0530

    deploy

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
-const app=require('./app')
+ const app=require('./app')
+//  dotenv.config()
+
+require('dotenv').config()
 
 const connect=require('./config/db')
+const port=process.env.PORT || 5555;
 
-app.listen(5555,async()=>{
+app.listen(port,async()=>{
     await connect()
-    console.log('listning port 5555')
+    console.log(`listening port ${port}`)
 })
+
+// const app=require('./app')
+
+// const connect=require('./config/db')
+
+// app.listen(5555,async()=>{
+//     await connect()
+//     console.log('listning port 5555')
+// })
